a5d517a7a4412fbb5614ae5832002112240aef64
polanj7 <polancojo@outlook.com>
2022-02-06 22:42:08 -0400

    ..
    ...

--- src/components/projects/FileForm.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function FileForm() {
-  return (
-      <>
-        <h3>Files</h3>
-      </>
-  );
-}
--- src/components/projects/TaksForm.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function TaksForm() {
-  return (
-    <>
-      <h3>Taks</h3>
-    </>
-  );
-}
